5063ebe58384673040393d7972b921469d1c7f9f
Muthuraj Muthulingam <muthurajmuthulingam@gmail.com>
2018-09-17 10:21:12 +0530

    Example Project update

--- Example/MMNetworkManager/ViewController.swift
+++ Example/MMNetworkManager/ViewController.swift
@@ -7,18 +7,36 @@
 //
 
 import UIKit
+import MMNetworkManager
+
+let kURLString = "http://www.mocky.io/v2/5a87a7b63200004a00f4e9a3"
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // reachablity  check
+        debugPrint("Network Status : \(MMNetworkManager.shared.isNetworkReachable)")
+        MMNetworkManager.shared.startNetworkStatusMonitoring()
+        MMNetworkManager.shared.networkStatusChanged = {(networkmanager, isConnect) in
+            debugPrint("Status Change :\(isConnect)")
+        }
+        loadDataFromServer()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    private func loadDataFromServer() {
+        guard let url = URL(string: kURLString) else { return }
+        let request = Request(from: url, params: nil, method: .get, responseType: .json, timeout: 30, hasHeader: false, headers: nil)
+        MMNetworkManager.shared.perform(Request: request) { response in
+            debugPrint("URL Response : \(response) and Error : \(String(describing: response.error))")
+        }
+    }
 }
 
